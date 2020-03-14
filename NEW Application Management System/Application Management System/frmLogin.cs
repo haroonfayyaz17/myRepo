374037bdd1570694b092fbdeeb9ae2b9626014d7
Your Name <you@example.com>
2020-03-14 21:57:22 +0500

    updated readme file

--- NEW Application Management System/Application Management System/frmLogin.cs
+++ NEW Application Management System/Application Management System/frmLogin.cs
@@ -20,7 +20,7 @@ namespace Application_Management_System
             InitializeComponent();
             obj.assign();
             con = obj.con;
-            MessageBox.Show("Welcome to Application Management System");
+            MessageBox.Show("Welcome to AMS");
         }
         void showApprovalMessage()
         {
